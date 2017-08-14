29686252fa5e4bd4acfabb89709298d3db2d733c
Dominik Zogg <dominik.zogg@gmail.com>
2017-08-14 21:42:13 +0200

    add provider

---
+++ src/Provider/ApiHttpProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\ApiHttp\Provider;
+
+use Chubbyphp\ApiHttp\Manager\RequestManager;
+use Chubbyphp\ApiHttp\Manager\ResponseManager;
+use Negotiation\Negotiator as ContentNegotiator;
+use Pimple\Container;
+
+final class ApiHttpProvider
+{
+    /**
+     * @param Container $container
+     */
+    public function register(Container $container)
+    {
+        $container['api-http.request.manager'] = function () use ($container) {
+            return new RequestManager(
+                $container['api-http.request.contentNegotiator'],
+                $container['deserializer'],
+                $container['deserializer.transformer']
+            );
+        };
+
+        $container['api-http.request.contentNegotiator'] = function () {
+            return new ContentNegotiator();
+        };
+
+        $container['api-http.request.manager'] = function () use ($container) {
+            return new ResponseManager(
+                $container['api-http.request.manager'],
+                $container['api-http.response.factory'],
+                $container['serializer'],
+                $container['serializer.transformer']
+            );
+        };
+
+        $container['api-http.response.factory'] = function () {
+            throw new \RuntimeException('Missing response factory, define service "api-http.response.factory"');
+        };
+    }
+}
