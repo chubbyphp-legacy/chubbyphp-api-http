0b922d736bf1546e12624bade88b71a7fb5d8d93
Dominik Zogg <dominik.zogg@gmail.com>
2017-09-06 15:02:33 +0200

    add tests for error

---
+++ tests/Error/ErrorTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Chubbyphp\Tests\ApiHttp\Error;
+
+use Chubbyphp\ApiHttp\Error\Error;
+
+/**
+ * @covers \Chubbyphp\ApiHttp\Error\Error
+ */
+final class ErrorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testScopeAndKey()
+    {
+        $error = new Error(
+            Error::SCOPE_BODY,
+            'body_not_deserializable'
+        );
+
+        self::assertSame(Error::SCOPE_BODY, $error->getScope());
+        self::assertSame('body_not_deserializable', $error->getKey());
+    }
+
+    public function testScopeAndKeyAndDetail()
+    {
+        $error = new Error(
+            Error::SCOPE_BODY,
+            'body_not_deserializable',
+            'the body is not deserializable'
+        );
+
+        self::assertSame(Error::SCOPE_BODY, $error->getScope());
+        self::assertSame('body_not_deserializable', $error->getKey());
+        self::assertSame('the body is not deserializable', $error->getDetail());
+    }
+
+    public function testScopeAndKeyAndDetailAndReference()
+    {
+        $error = new Error(
+            Error::SCOPE_BODY,
+            'body_not_deserializable',
+            'the body is not deserializable',
+            'model'
+        );
+
+        self::assertSame(Error::SCOPE_BODY, $error->getScope());
+        self::assertSame('body_not_deserializable', $error->getKey());
+        self::assertSame('the body is not deserializable', $error->getDetail());
+        self::assertSame('model', $error->getReference());
+    }
+
+    public function testScopeAndKeyAndDetailAndReferenceAndArguments()
+    {
+        $error = new Error(
+            Error::SCOPE_BODY,
+            'body_not_deserializable',
+            'the body is not deserializable',
+            'model',
+            ['contentType' => 'application/json', 'body' => '{{""}']
+        );
+
+        self::assertSame(Error::SCOPE_BODY, $error->getScope());
+        self::assertSame('body_not_deserializable', $error->getKey());
+        self::assertSame('the body is not deserializable', $error->getDetail());
+        self::assertSame('model', $error->getReference());
+        self::assertEquals(['contentType' => 'application/json', 'body' => '{{""}'], $error->getArguments());
+    }
+}
