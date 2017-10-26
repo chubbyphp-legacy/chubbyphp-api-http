946d9b9ac7ec0118102e89c5809726ff574ecd6c
Dominik Zogg <dominik.zogg@gmail.com>
2017-10-26 18:46:18 +0200

    first steps to v2

--- src/Manager/ResponseManagerInterface.php
+++ src/Manager/ResponseManagerInterface.php
@@ -5,99 +5,107 @@ declare(strict_types=1);
 namespace Chubbyphp\ApiHttp\Manager;
 
 use Chubbyphp\ApiHttp\Error\ErrorInterface;
+use Chubbyphp\Serialization\Normalizer\NormalizerContext;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 interface ResponseManagerInterface
 {
     /**
-     * @param Request     $request
-     * @param int         $code
-     * @param string      $accept
-     * @param object|null $object
-     *
+     * @param object|null            $object
+     * @param string                 $accept
+     * @param int                    $status
+     * @param NormalizerContext|null $context
      * @return Response
      */
-    public function createResponse(Request $request, int $code, string $accept, $object = null): Response;
-
-    /**
-     * @param Request        $request
-     * @param int            $code
-     * @param string         $accept
-     * @param ErrorInterface $error
-     *
-     * @return Response
-     */
-    public function createResponseByError(Request $request, int $code, string $accept, ErrorInterface $error): Response;
-
-    /**
-     * @param Request $request
-     * @param string  $accept
-     * @param string  $contentType
-     *
-     * @return Response
-     */
-    public function createBodyNotDeserializableResponse(Request $request, string $accept, string $contentType): Response;
-
-    /**
-     * @param Request $request
-     * @param string  $accept
-     * @param string  $type
-     * @param array   $arguments
-     *
-     * @return Response
-     */
-    public function createPermissionDeniedResponse(
-        Request $request,
+    public function create(
+        $object,
         string $accept,
+        int $status = 200,
+        NormalizerContext $context = null
+    ): Response;
+
+    /**
+     * @param string $url
+     * @param int    $code
+     * @return Response
+     */
+    public function createRedirect(string $url, int $code = 307): Response;
+
+    /**
+     * @param ErrorInterface         $error
+     * @param string                 $accept
+     * @param int                    $status
+     * @param NormalizerContext|null $context
+     * @return Response
+     */
+    public function createByError(
+        ErrorInterface $error,
+        string $accept,
+        int $status = 400,
+        NormalizerContext $context = null
+    ): Response;
+
+    /**
+     * @param string                 $type
+     * @param array                  $arguments
+     * @param string                 $accept
+     * @param NormalizerContext|null $context
+     * @return Response
+     */
+    public function createNotAuthenticated(
         string $type,
-        array $arguments
+        array $arguments,
+        string $accept,
+        NormalizerContext $context = null
+    ): Response;
+
+    /**
+     * @param string                 $type
+     * @param array                  $arguments
+     * @param string                 $accept
+     * @param NormalizerContext|null $context
+     * @return Response
+     */
+    public function createNotAuthorized(
+        string $type,
+        array $arguments,
+        string $accept,
+        NormalizerContext $context = null
+    ): Response;
+
+    /**
+     * @param string                 $type
+     * @param array                  $arguments
+     * @param string                 $accept
+     * @param NormalizerContext|null $context
+     * @return Response
+     */
+    public function createResourceNotFound(
+        string $type,
+        array $arguments,
+        string $accept,
+        NormalizerContext $context = null
     ): Response;
 
     /**
      * @param Request $request
-     * @param string  $accept
-     * @param string  $type
-     * @param array   $arguments
      *
      * @return Response
      */
-    public function createResourceNotFoundResponse(
+    public function createAcceptNotSupported(Request $request): Response;
+
+    /**
+     * @param Request                $request
+     * @param string                 $accept
+     * @param array                  $supportedContentTypes
+     * @param NormalizerContext|null $context
+     * @return Response
+     */
+    public function createContentTypeNotSupported(
         Request $request,
         string $accept,
-        string $type,
-        array $arguments
-    ): Response;
-
-    /**
-     * @param Request $request
-     *
-     * @return Response
-     */
-    public function createAcceptNotSupportedResponse(Request $request): Response;
-
-    /**
-     * @param Request $request
-     * @param string  $accept
-     *
-     * @return Response
-     */
-    public function createContentTypeNotSupportedResponse(Request $request, string $accept): Response;
-
-    /**
-     * @param Request $request
-     * @param string  $accept
-     * @param string  $scope
-     * @param string  $type
-     * @param array   $errors
-     *
-     * @return Response
-     */
-    public function createValidationErrorResponse(
-        Request $request,
-        string $accept,
-        string $scope,
-        string $type,
-        array $errors
+        array $supportedContentTypes,
+        NormalizerContext $context = null
     ): Response;
 }
